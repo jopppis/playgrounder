5c3e5504a2130ead3be6b9fc4284fed91bb47a25
Ville Joensuu <jopppis@iki.fi>
2025-02-19 21:57:11 +0200

    Remove /signup route

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,10 @@
 import { Route, Routes } from 'react-router-dom'
 import App from './App'
-import SignUp from './components/Auth/SignUp'
 
 export default function Router() {
   return (
     <Routes>
       <Route path="/" element={<App />} />
-      <Route path="/signup" element={<SignUp />} />
     </Routes>
   )
 }
