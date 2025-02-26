d65692ac83cba36b08c6550ebf8d5d9ec50de803
Ville Joensuu <jopppis@iki.fi>
2025-02-26 23:54:18 +0200

    Improve supabase setup

--- supabase/migrations/003_add_city_column.sql
+++ supabase/migrations/003_add_city_column.sql
@@ -3,24 +3,3 @@ ALTER TABLE playgrounds ADD COLUMN city TEXT;
 
 -- Create an index on city for faster filtering
 CREATE INDEX idx_playgrounds_city ON playgrounds(city);
-
--- Update cities based on address patterns
-UPDATE playgrounds
-SET city = 'Helsinki'
-WHERE address LIKE '%Helsinki%'
-  AND city IS NULL;
-
-UPDATE playgrounds
-SET city = 'Espoo'
-WHERE address LIKE '%Espoo%'
-  AND city IS NULL;
-
-UPDATE playgrounds
-SET city = 'Vantaa'
-WHERE address LIKE '%Vantaa%'
-  AND city IS NULL;
-
-UPDATE playgrounds
-SET city = 'Kauniainen'
-WHERE address LIKE '%Kauniainen%'
-  AND city IS NULL;
