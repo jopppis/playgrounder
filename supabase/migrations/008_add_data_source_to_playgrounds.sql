9c17020e2feccc83cfccc5a3c1a1145dda352feb
Ville Joensuu <jopppis@iki.fi>
2025-03-03 07:58:21 +0200

    Add data source to user filters also

--- supabase/migrations/008_add_data_source_to_playgrounds.sql
+++ supabase/migrations/008_add_data_source_to_playgrounds.sql
@@ -4,3 +4,7 @@ CREATE TYPE playground_data_source_type AS ENUM ('municipality', 'osm');
 -- Add data_source column
 ALTER TABLE playgrounds
 ADD COLUMN data_source playground_data_source_type;
+
+-- Add data_source to user_filters
+ALTER TABLE user_filters
+ADD COLUMN data_source playground_data_source_type;
